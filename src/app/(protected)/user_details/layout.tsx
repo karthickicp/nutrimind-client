aac762876cc56f34c98ec4bfb0cd501256c3c718
Karthick Raja <karthickicp15@gmail.com>
2025-12-17 21:57:00 +0530

    refactor: provider setup updated

--- src/app/(protected)/user_details/layout.tsx
+++ src/app/(protected)/user_details/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "@/app/globals.css";
-import UserDetailsProvider from "../layout/provider";
+import UserDetailsProvider from "@/providers/user-details-provider";
 
 const popins = Poppins({
   variable: "--font-poppins",
